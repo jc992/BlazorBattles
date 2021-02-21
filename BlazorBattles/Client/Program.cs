f5366ac5ff3758a766206e07e0e7c177f285ba97
Joao Tavares <jc.992@hotmail.com>
2021-02-21 10:50:56 -0100

    Using BlazoredToast to display success or failed messages

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBattles.Client.Services;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace BlazorBattles.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddBlazoredToast();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
