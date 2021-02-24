0a7d56ab9a6b10434ebeda930eaaf645c4b7f446
Joao Tavares <jc.992@hotmail.com>
2021-02-24 15:08:54 -0100

    Added logout option.

--- BlazorBattles/Client/CustomAuthStateProvider.cs
+++ BlazorBattles/Client/CustomAuthStateProvider.cs
@@ -17,6 +17,8 @@ namespace BlazorBattles.Client
         }
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
             if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
             {
                 var identity = new ClaimsIdentity(new[]
@@ -25,13 +27,11 @@ namespace BlazorBattles.Client
             }, "Test Auth Type");
 
                 var user = new ClaimsPrincipal(identity);
-                var state = new AuthenticationState(user);
-
-                NotifyAuthenticationStateChanged(Task.FromResult(state));
-                return state;
+                state = new AuthenticationState(user);
             }
 
-            return new AuthenticationState(new ClaimsPrincipal());
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+            return state;
         }
     }
 }
