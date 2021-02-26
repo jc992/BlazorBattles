556c7f490c7fa31751ed88e1eaaf35eef162f6e0
Joao Tavares <jc.992@hotmail.com>
2021-02-26 15:47:57 -0100

    Added AuthController

--- BlazorBattles/Server/Data/IAuthRepository.cs
+++ BlazorBattles/Server/Data/IAuthRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlazorBattles.Server.Data
 {
-    interface IAuthRepository
+    public interface IAuthRepository
     {
         Task<ServiceResponse<int>> Register(User user, string password);
         Task<ServiceResponse<string>> Login(string email, string password);
