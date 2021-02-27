f386aa2645a31fed04c0ad45464e2ffe9d986436
Joao Tavares <jc.992@hotmail.com>
2021-02-27 11:07:29 -0100

    User & UserUnit Models refactored, new database migration successful with table for userUnits.

--- BlazorBattles/Server/Data/DataContext.cs
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -15,6 +15,6 @@ namespace BlazorBattles.Server.Data
         }
         public DbSet<Unit> Units { get; set; }
         public DbSet<User> Users { get; set; }
-
+        public DbSet<UserUnit> UserUnits { get; set; }
     }
 }
